6231c55954647aba58644c0a6a4ba7d9065938bc
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2022-01-26 13:34:13 +0100

    fun3 -> git function

---
+++ src/functions/fun3-git-node-public/handler.js
@@ -0,0 +1,37 @@
+const fibonacci = require ('fibonacci');
+module.exports = {
+    main: async function (event, context) {
+      const dataIn = sanitizeInput(event.data);
+      console.log(`Will wait ${waitTime}`);
+        var payload = {
+            ...dataIn,
+            check_in: {
+                ...dataIn.check_in,
+                fun3: new Date()
+            },
+            fibo: {
+            ...dataIn.fibo,
+            fun3: fibonacci.iterate(between(parseInt(process.env['FIBONACCI_MIN'],10),parseInt(process.env['FIBONACCI_MAX'],10)))
+            }
+        };
+   
+        var eventOut=event.buildResponseCloudEvent(payload.uuid,process.env['PUSH_EVENT_TYPE'],payload);
+        event.publishCloudEvent(eventOut);
+        console.log(`Payload [${payload.uuid}] pushed to ${process.env['PUSH_EVENT_TYPE']}`,payload)
+      return payload;
+    },
+  };
+
+
+  function between(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  
+  function sanitizeInput(input) {
+    let sanitised = input;
+    console.log("Type of incomming input : "+ typeof sanitised);
+    if(typeof sanitised === 'string'){
+        return JSON.parse(sanitised);
+    }
+    return sanitised;
+}
